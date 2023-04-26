075731178a129f0eedb8dea1e44a37954f9824f7
Anton Maurovic <anton@foogle.com>
2023-04-26 13:58:50 +0200

    Change to support configurable operand and product result width via OP_NIBBLES

--- src/product.v
+++ src/product.v
@@ -4,15 +4,19 @@ module algofoogle_product (
     input   [7:0]   io_in,
     output  [7:0]   io_out
 );
+    localparam OP_NIBBLES = 3; // This determines both the size of the input, and the product output.
+    localparam OP_BITS = OP_NIBBLES*4;
+    localparam MUL_BITS = (OP_BITS*2);
+
     wire        clk    = io_in[0];
     wire        reset  = io_in[1];
     wire [3:0]  nibble = io_in[7:4];
 
-    reg [2:0]   state;
+    reg [3:0]   state;  //SMELL: This should be sized to fit actual number of required states (based on OP_NIBBLES).
 
-    reg [15:0]  product;
+    reg [MUL_BITS-1:0]  product;
 
-    assign io_out = product[15:8];
+    assign io_out = product[MUL_BITS-1:MUL_BITS-8];
 
     always @(posedge clk) begin
         if (reset) begin
@@ -20,17 +24,18 @@ module algofoogle_product (
             product <= 0;
             state <= 0;
         end else begin
-            if (state < 4) begin
-                // We're clocking in 4 nibbles (2 byte values):
-                product <= {product[11:0],nibble};
-            end else if (state == 4) begin
+            if (state < OP_NIBBLES*2) begin
+                // We're clocking in nibbles for each of our two operands:
+                product <= {product[MUL_BITS-4-1:0],nibble};
+            end else if (state == OP_NIBBLES*2) begin
                 // We've got the data we need; now calculate the product:
-                product <= {8'b0,product[7:0]} * {8'b0,product[15:8]};
+                // product <= { {OP_BITS{1'b0}}, product[OP_BITS-1:0] } * { {OP_BITS{1'b0}}, product[MUL_BITS-1:OP_BITS] };
+                product <= { product[OP_BITS-1:0] } * { product[MUL_BITS-1:OP_BITS] };
             end else begin
                 // Start clocking out the result, as 2 bytes.
                 product <= product << 8;
             end
-            state <= (state==5) ? 0 : state+1;
+            state <= (state==OP_NIBBLES*3-1) ? 0 : state+1;
         end
     end
 
--- src/test.py
+++ src/test.py
@@ -3,19 +3,22 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
+OP_NIBBLES = 3
+
+
 async def send_nibble(dut, value):
     dut.nibble.value = value
     await RisingEdge(dut.clk)
     await FallingEdge(dut.clk)
 
-async def send_byte(dut, value):
-    for i in range(2):
-        await send_nibble(dut, (value & 0xF0) >> 4)
+async def send_operand(dut, value):
+    for i in range(OP_NIBBLES):
+        await send_nibble(dut, (value >> ((OP_NIBBLES-1)*4))&0xF)
         value <<= 4
 
-async def get_word(dut):
+async def get_product(dut):
     word = 0
-    for i in range(2):
+    for i in range(OP_NIBBLES): #NOTE: This gets a BYTE at a time (i.e. product width is sum of 2 operand widths).
         await RisingEdge(dut.clk)
         await FallingEdge(dut.clk)
         word <<= 8
@@ -42,18 +45,54 @@ async def test_product(dut):
 
     # Test multiplies
     data = [
-        [10,10],
-        [123,234],
-        [255,255],
-        [0,189],
-        [88,111]
+        # Tests that fit in 1 nibble (4-bit operands):
+        [
+            [    0,     0],
+            [    0,     1],
+            [    1,     0],
+            [    1,     1],
+            [    1,     2],
+            [    2,     3],
+            [    9,     3],
+            [    3,     9],
+            [   15,    14],
+            [   13,    12],
+            [   12,     9],
+            [   10,     5],
+            [   10,    10],
+            [   11,    11],
+        ],
+        # Tests that fit in 2 nibbles (8-bit operands):
+        [
+            [  123,   234],
+            [  255,   255],
+            [    0,   189],
+            [   88,   111],
+        ],
+        # Tests that fit in 3 nibbles (12-bit operands):
+        [
+            [    0,  4095],
+            [ 4095,     0],
+            [ 4095,     1],
+            [    1,  4095],
+            [ 4095,  4095],
+            [ 1234,  3210],
+            [   25,  3456],
+            [  167,   256],
+            [  256,  1024],
+        ],
     ]
 
-    for pair in data:
-        a = pair[0]
-        b = pair[1]
-        await send_byte(dut, a)
-        await send_byte(dut, b)
-        p = await get_word(dut)
-        dut._log.info("Product {a:09d}*{b:09d}:".format(a=a, b=b))
-        assert p == a*b, "Expected {ab:08x} but got {p:08x}".format(ab=a*b, p=p)
+    n = 0
+    for batch in data:
+        n += 1
+        if n > OP_NIBBLES: break
+        dut._log.info("Testing {}-nibble operands".format(n))
+        for pair in batch:
+            a = pair[0]
+            b = pair[1]
+            await send_operand(dut, a)
+            await send_operand(dut, b)
+            p = await get_product(dut)
+            dut._log.info("Product {a:9d} * {b:9d} => {p:9d}".format(a=a, b=b, p=p))
+            assert p == a*b, "Expected {ab:08x} but got {p:08x}".format(ab=a*b, p=p)
